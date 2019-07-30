4e63a1bc9e61b8e8220e0d0113abf69582e3fa51
alphacalcu <alphacalcu@163.com>
2019-07-30 10:46:08 +0800

    调整目录结构

--- setup.py
+++ setup.py
@@ -18,26 +18,26 @@ if "--line_trace" in sys.argv:
     sys.argv.remove("--line_trace")
 else:
     line_trace = False
-    
+
 
 ext_modules = [
-    "Utilities/Asserts.pyx",
-    "Utilities/Tools.pyx",
-    "Math/MathConstants.pyx",
-    "Math/ErrorFunction.pyx",
-    "Math/udfs.pyx",
-    "Math/Distributions/NormalDistribution.pyx",
-    "Math/Distributions/norm.pyx",
-    "Math/Accumulators/impl.pyx",
-    "Math/Accumulators/IAccumulators.pyx",
-    "Math/Accumulators/StatelessAccumulators.pyx",
-    "Math/Accumulators/StatefulAccumulators.pyx",
-    "Analysis/SecurityValueHolders.pyx",
-    "Analysis/SeriesValues.pyx",
-    "Analysis/transformer.pyx",
-    "Analysis/CrossSectionValueHolders.pyx",
-    "Analysis/TechnicalAnalysis/StatefulTechnicalAnalysers.pyx",
-    "Analysis/TechnicalAnalysis/StatelessTechnicalAnalysers.pyx",
+    "sentry/Utilities/Asserts.pyx",
+    "sentry/Utilities/Tools.pyx",
+    "sentry/Math/MathConstants.pyx",
+    "sentry/Math/ErrorFunction.pyx",
+    "sentry/Math/udfs.pyx",
+    "sentry/Math/Distributions/NormalDistribution.pyx",
+    "sentry/Math/Distributions/norm.pyx",
+    "sentry/Math/Accumulators/impl.pyx",
+    "sentry/Math/Accumulators/IAccumulators.pyx",
+    "sentry/Math/Accumulators/StatelessAccumulators.pyx",
+    "sentry/Math/Accumulators/StatefulAccumulators.pyx",
+    "sentry/Analysis/SecurityValueHolders.pyx",
+    "sentry/Analysis/SeriesValues.pyx",
+    "sentry/Analysis/transformer.pyx",
+    "sentry/Analysis/CrossSectionValueHolders.pyx",
+    "sentry/sentry/Analysis/TechnicalAnalysis/StatefulTechnicalAnalysers.pyx",
+    "sentry/Analysis/TechnicalAnalysis/StatelessTechnicalAnalysers.pyx",
 ]
 
 def generate_extensions(ext_modules, line_trace=False):
@@ -63,10 +63,10 @@ if platform.system() != "Windows":
     n_cpu = multiprocessing.cpu_count()
 else:
     n_cpu = 0
-    
 
-ext_modules_settings = cythonize(generate_extensions(ext_modules, line_trace), 
-                                 compiler_directives={'embedsignature': True, 'linetrace': line_trace}, 
+
+ext_modules_settings = cythonize(generate_extensions(ext_modules, line_trace),
+                                 compiler_directives={'embedsignature': True, 'linetrace': line_trace},
                                  nthreads=n_cpu)
 
 setup(
@@ -74,4 +74,3 @@ setup(
     ext_modules = ext_modules_settings,
     include_dirs=[np.get_include()]
 )
-
